b30ac7b36cbb2e56e7bd1462e8f8c7f05b9c48d2
Chau Pham <ncp9988@gmail.com>
2022-03-02 17:36:37 -0600

    convert app into SPA

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -24,11 +25,15 @@ function App() {
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
       ></Nav>
-      <main>
+
+      {!contactSelected ? (
+        <>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </>
+      ) : (
         <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-      </main>
+      )}
     </div>
   );
 }
